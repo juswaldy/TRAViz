1ee22dfefdbf1f180fab999975e40ef7622a8ddf
Juswaldy Jusman <juswaldy.jusman@twu.ca>
2017-11-09 16:41:26 -0800

    Add a few hebrew tests

---
+++ test/bible-hebrew/data/bible-hebrew.js
@@ -0,0 +1,20 @@
+var fromPassage = "Psalm 107:36";
+var toPassage = "Psalm 107:36";
+var connectionType = "all";
+var fontSizeMin = 23;
+
+var bibleEditions = [
+	"WLC",
+	"4Q88"
+];
+
+var editionIds = [
+	"WLC",
+	"4Q88"
+];
+
+var bibleVerses = [[
+	"ויושב שם רעבים ויכוננו עיר מושב",
+	"ווישב שם עם רב ויכוננו  ערי מושב"
+]
+];
