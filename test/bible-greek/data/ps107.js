96075217d06ea1fdfd93431415fab1875253c7fc
Juswaldy Jusman <juswaldy.jusman@twu.ca>
2017-11-09 16:46:08 -0800

    Check in greek test data

---
+++ test/bible-greek/data/ps107.js
@@ -0,0 +1,21 @@
+var fromPassage = "Psalm 107:36";
+var toPassage = "Psalm 107:36";
+var connectionType = "all";
+var fontSizeMin = 23;
+
+var bibleEditions = [
+	"Rahfls",
+	"Ra2110"	
+];
+
+var editionIds = [
+	"Rahfls",
+	"Ra2110"
+];
+
+var bibleVerses = [
+[
+	"και κατωκισεν εκει πεινωντας , και συνεστησαντο πολιν κατοικεσιας",
+	"καιτωκισεν εκει πεινωντας και συνεστησατο πολιν κατοικησιας"
+]
+];
